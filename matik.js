9b9ccc2c307ea8c733a1844a9cbefda7bddbbcc0
Ivan Sibinovic <sibinovic.ivan90@gmail.com>
2020-12-21 19:00:58 +0100

    bodovi
    dodati bodovi

--- matik.js
+++ matik.js
@@ -1,16 +1,19 @@
 var equationDisplay = document.querySelector('#equationDisplay')
 var randomNumber1 = Math.floor(Math.random() *10)+1;
 var randomNumber2 = Math.floor(Math.random() *5)+1;
+var counter = 0;
+var wrong = 0;
 if(randomNumber1 + randomNumber2 >10) {
 	var randomNumber1 = Math.floor(Math.random() *2)+1;
 	var randomNumber2 = Math.floor(Math.random() *6);
 }
 var operationDisplay = randomNumber1 + ' + ' + randomNumber2 + ' = ';
 var result =  randomNumber1 + randomNumber2;
-var resultDisplay = document.querySelector('#resultDisplay');
+var resultDisplay = document.querySelector('.resultDisplay');
 resultDisplay.textContent = result;
 equationDisplay.textContent = operationDisplay;
 var answerButton = document.querySelectorAll('.answerButton');
+var correctAnswers = document.querySelectorAll('.count');
 
 //generate random possible solutions
 /*
@@ -42,12 +45,28 @@ for (var i = 0; i < answerButton.length; i++) {
 		if(parseInt(this.textContent) === result ) {
 			document.querySelector(".correct").classList.add("rightAnswer");
 			document.querySelector(".resultDisplay").classList.add("appear");
+			bravo(counter);
+			//document.querySelector(".count").classList.add("correctAnswers");
+			counter++;
 			setTimeout(function(){
+				document.querySelector(".resultDisplay").style.display = "none";
 				reset();
 			}, 1000);
-			
+			if (wrong+counter===5) {
+				alert("GOTOVO!!!");
+				//tu ce da ide povratak na prethodnu stranu
+			}
 		} else {
-			this.classList.add("fadeOut");
+			wrong++;
+			document.querySelector(".wrong").classList.add("rightAnswer");
+			setTimeout(function(){
+				document.querySelector(".resultDisplay").style.display = "none";
+				reset();
+			}, 1000);
+			if (wrong+counter===5) {
+				alert("GOTOVO!!!");
+				//tu ce da ide povratak na prethodnu stranu
+			}
 		}
 	})
 }
@@ -61,7 +80,7 @@ function reset () {
 	}
 	operationDisplay = randomNumber1 + ' + ' + randomNumber2 + ' = ';
 	result =  randomNumber1 + randomNumber2;
-	resultDisplay = document.querySelector('#resultDisplay');
+	resultDisplay = document.querySelector('.resultDisplay');
 	resultDisplay.textContent = result;
 	equationDisplay.textContent = operationDisplay;
 	answerButton = document.querySelectorAll('.answerButton');
@@ -81,8 +100,14 @@ function reset () {
 	answerButton[randomSolution].textContent = result;
 
 	document.querySelector(".correct").classList.remove('rightAnswer');
+	 document.querySelector(".wrong").classList.remove("rightAnswer");
 	document.querySelector(".resultDisplay").classList.remove("appear");
+	 document.querySelector(".resultDisplay").style.display = "inline-block";
 	for(var i = 0; i < answerButton.length; i++){
 		answerButton[i].classList.remove('fadeOut');
 	}
+}
+
+function bravo(count) {
+	correctAnswers[count].style.backgroundColor = "#688d01"
 }
